16b8551a06f13fd85d81a0c7cbda031f74bc4d1c
Franklin S Cooper Jr <fcooper@ti.com>
2018-09-18 13:56:45 -0500

    update install

--- beaglebone/configuration/install.sh
+++ beaglebone/configuration/install.sh
@@ -5,5 +5,5 @@ sudo cp -r ../workshop/bopit/webpage/* /var/www/html/workshop/bopit
 
 sudo cp ../workshop/workshop.py /usr/local/lib/python2.7/dist-packages/workshop.py
 
-sudo apt-get install python-flask
-sudo pip install flask-cors
+sudo apt-get install -y python-flask
+yes | sudo pip install flask-cors
